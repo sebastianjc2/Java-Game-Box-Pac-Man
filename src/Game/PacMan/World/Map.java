a37f682b19b535e9acf84901e16a8064e382bb8b
sebastianjc2 <sebastian.caballero@upr.edu>
2020-04-07 16:55:48 -0400

    fixed pull error

--- src/Game/PacMan/World/Map.java
+++ src/Game/PacMan/World/Map.java
@@ -13,108 +13,110 @@ import java.util.Random;
 
 public class Map {
 
-    ArrayList<BaseStatic> blocksOnMap;
-    ArrayList<BaseDynamic> enemiesOnMap;
-    Handler handler;
-    private double bottomBorder;
-    private Random rand;
-    private int mapBackground;
-    ArrayList<BaseDynamic> toAdd = new ArrayList<>();
+	ArrayList<BaseStatic> blocksOnMap;
+	ArrayList<BaseDynamic> enemiesOnMap;
+	Handler handler;
+	private double bottomBorder;
+	private Random rand;
+	private int mapBackground;
+	ArrayList<BaseDynamic> toAdd = new ArrayList<>();
 
-    public Map(Handler handler) {
-        this.handler=handler;
-        this.rand = new Random();
-        this.blocksOnMap = new ArrayList<>();
-        this.enemiesOnMap = new ArrayList<>();
-        bottomBorder=handler.getHeight();
-        this.mapBackground = this.rand.nextInt(6);
-    }
+	public Map(Handler handler) {
+		this.handler=handler;
+		this.rand = new Random();
+		this.blocksOnMap = new ArrayList<>();
+		this.enemiesOnMap = new ArrayList<>();
+		bottomBorder=handler.getHeight();
+		this.mapBackground = this.rand.nextInt(6);
+	}
 
-    public void addBlock(BaseStatic block){
-        blocksOnMap.add(block);
-    }
+	public void addBlock(BaseStatic block){
+		blocksOnMap.add(block);
+	}
 
-    public void addEnemy(BaseDynamic entity){
+	public void addEnemy(BaseDynamic entity){
 
-        enemiesOnMap.add(entity);
+		enemiesOnMap.add(entity);
 
-    }
+	}
 
-    public void toAdd(BaseDynamic x){
-        toAdd.add(x);
-    }
+	public void toAdd(BaseDynamic x){
+		toAdd.add(x);
+	}
 
-    public void drawMap(Graphics2D g2) {
-        for (BaseStatic block:blocksOnMap) {
-        	if (block instanceof BigDot) {
+	public void drawMap(Graphics2D g2) {
+		for (BaseStatic block:blocksOnMap) {
+			if (block instanceof BigDot) {
 				g2.drawImage(handler.getPacman().bigDotBlink.getCurrentFrame(), block.x, block.y, block.width, block.height, null);
 			}
 			else {
 				g2.drawImage(block.sprite, block.x, block.y, block.width, block.height, null);
 			}
-        }
-        for (BaseDynamic entity:enemiesOnMap) {
-            if (entity instanceof PacMan) {
-                if(!entity.ded) {
-                    switch (((PacMan) entity).facing) {
-                        case "Right":
-                            g2.drawImage(((PacMan) entity).rightAnim.getCurrentFrame(), entity.x, entity.y, entity.width, entity.height, null);
-                            break;
-                        case "Left":
-                            g2.drawImage(((PacMan) entity).leftAnim.getCurrentFrame(), entity.x, entity.y, entity.width, entity.height, null);
-                            break;
-                        case "Up":
-                            g2.drawImage(((PacMan) entity).upAnim.getCurrentFrame(), entity.x, entity.y, entity.width, entity.height, null);
-                            break;
-                        case "Down":
-                            g2.drawImage(((PacMan) entity).downAnim.getCurrentFrame(), entity.x, entity.y, entity.width, entity.height, null);
-                            break;
-                    }
-                }
-                else{
-                    g2.drawImage(((PacMan) entity).pacmanDeathAnim.getCurrentFrame(), entity.x, entity.y, entity.width, entity.height, null);
-                }
-            }
-            else if (entity instanceof Ghost) {
-                switch (((Ghost) entity).facing) {
-                    case "Right":
-                        g2.drawImage(((Ghost) entity).rightAnim.getCurrentFrame(), entity.x, entity.y, entity.width, entity.height, null);
-                        break;
-                    case "Left":
-                        g2.drawImage(((Ghost) entity).leftAnim.getCurrentFrame(), entity.x, entity.y, entity.width, entity.height, null);
-                        break;
-                    case "Up":
-                        g2.drawImage(((Ghost) entity).upAnim.getCurrentFrame(), entity.x, entity.y, entity.width, entity.height, null);
-                        break;
-                    case "Down":
-                        g2.drawImage(((Ghost) entity).downAnim.getCurrentFrame(), entity.x, entity.y, entity.width, entity.height, null);
-                        break;
-                }
-            }
-            else {
-                g2.drawImage(entity.sprite, entity.x, entity.y, entity.width, entity.height, null);
-            }
-        }
+		}
+		for (BaseDynamic entity:enemiesOnMap) {
+			if (entity instanceof PacMan) {
+				if(!entity.ded) {
+					switch (((PacMan) entity).facing) {
+					case "Right":
+						g2.drawImage(((PacMan) entity).rightAnim.getCurrentFrame(), entity.x, entity.y, entity.width, entity.height, null);
+						break;
+					case "Left":
+						g2.drawImage(((PacMan) entity).leftAnim.getCurrentFrame(), entity.x, entity.y, entity.width, entity.height, null);
+						break;
+					case "Up":
+						g2.drawImage(((PacMan) entity).upAnim.getCurrentFrame(), entity.x, entity.y, entity.width, entity.height, null);
+						break;
+					case "Down":
+						g2.drawImage(((PacMan) entity).downAnim.getCurrentFrame(), entity.x, entity.y, entity.width, entity.height, null);
+						break;
+					}
+				}
+				else{
+					g2.drawImage(((PacMan) entity).pacmanDeathAnim.getCurrentFrame(), entity.x, entity.y, entity.width, entity.height, null);
+				}
+			}
+			else if (entity instanceof Ghost) {
+					switch (((Ghost) entity).facing) {
+					case "Right":
+						g2.drawImage(((Ghost) entity).rightAnim.getCurrentFrame(), entity.x, entity.y, entity.width, entity.height, null);
+						break;
+					case "Left":
+						g2.drawImage(((Ghost) entity).leftAnim.getCurrentFrame(), entity.x, entity.y, entity.width, entity.height, null);
+						break;
+					case "Up":
+						g2.drawImage(((Ghost) entity).upAnim.getCurrentFrame(), entity.x, entity.y, entity.width, entity.height, null);
+						break;
+					case "Down":
+						g2.drawImage(((Ghost) entity).downAnim.getCurrentFrame(), entity.x, entity.y, entity.width, entity.height, null);
+						break;
+					}
+				
+			}
 
-    }
-
-    public ArrayList<BaseStatic> getBlocksOnMap() {
-        return blocksOnMap;
-    }
-
-    public ArrayList<BaseDynamic> getEnemiesOnMap() {
-        return enemiesOnMap;
-    }
-
-    public ArrayList<BaseDynamic> getEnemiesToAdd() {
-        return toAdd;
-    }
+			else {
+				g2.drawImage(entity.sprite, entity.x, entity.y, entity.width, entity.height, null);
+			}
+		}
+	}
 
 
-    public double getBottomBorder() {
-        return bottomBorder;
-    }
+	public ArrayList<BaseStatic> getBlocksOnMap() {
+		return blocksOnMap;
+	}
 
-    public void reset() {
-    }
+	public ArrayList<BaseDynamic> getEnemiesOnMap() {
+		return enemiesOnMap;
+	}
+
+	public ArrayList<BaseDynamic> getEnemiesToAdd() {
+		return toAdd;
+	}
+
+
+	public double getBottomBorder() {
+		return bottomBorder;
+	}
+
+	public void reset() {
+	}
 }
